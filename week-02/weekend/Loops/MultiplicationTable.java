0e82b7fbd2e8cb2ab1e40f4ebab0e34072433aac
falonpok <91430165+falonpok@users.noreply.github.com>
2021-10-23 23:04:26 +0200

    MultiplicationTable upload

---
+++ week-02/weekend/Loops/MultiplicationTable.java
@@ -0,0 +1,32 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        /*
+         Hozz létre egy egész szám típusú, number elnevezésű változót
+         Írj egy programot, ami a standard input-ból kér egy számot és
+         elmenti azt a number változóban, majd
+         kiírja a number változóban megadott számhoz a szorzótáblát
+         Például ha a number értéke 15, akkor ezt írná ki:
+         1 * 15 = 15
+         2 * 15 = 30
+         3 * 15 = 45
+         4 * 15 = 60
+         5 * 15 = 75
+         6 * 15 = 90
+         7 * 15 = 105
+         8 * 15 = 120
+         9 * 15 = 135
+         10 * 15 = 150
+        */
+        System.out.println("Írj be egy egész számot!");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "*" + number + "=" + (i * number) );
+        }
+
+    }
+}
