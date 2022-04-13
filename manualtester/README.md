9fc8aeffb44d191326188bb281b0cdb6ef2f7c8e
falonpok <egona001@gmail.com>
2022-04-13 02:15:00 +0200

    delete

--- manualtester/README.md
+++
@@ -1,2 +0,0 @@
-# falonpok
-CsKriszta - Tigers repo
--- manualtester/week-02/day-3/src/javagyakorlas/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package javagyakorlas;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Kiírja, hogy "Hello World!"
-        System.out.println("Hello World!");
-    }
-}
